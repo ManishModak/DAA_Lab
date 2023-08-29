8047d4c3a20c83b56a7224c7864bc0db242ce6cb
manish <manishmodak90@gmail.com>
2023-08-29 19:34:42 +0530

    completed Fractional Knapsack

--- Lab1/FractionalKnapsack.cpp
+++ Lab1/FractionalKnapsack.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std ;
 
 struct Item 
@@ -9,18 +9,36 @@ struct Item
 
 bool comparator(Item A, Item B)
 {
-	
+	double ratio1 = (double)A.value/(double)A.weight ;
+	double ratio2 = (double)B.value/(double)B.weight ;
+	return ratio1 > ratio2 ;
 };
 
 int knapSack(Item data[], int size, int maxWeight)
 {
 	sort(data,data+size,comparator);
-	return 0 ;
+
+	double maxValue = 0.0 ;
+
+	for(int i = 0 ; i < size ; i++)
+	{
+		if(data[i].weight < maxWeight)
+		{
+			maxWeight -=  data[i].weight ;
+			maxValue += data[i].value ;
+		}
+		else
+		{
+			maxValue += data[i].value*(maxWeight/data[i].weight) ;
+		}
+	}
+
+	return maxValue ;
 }
 
 int main()
 {
-	Item data[] = {(100,40),(500,30),(50,50)} ;
+	Item data[] = {{100, 20}, {60, 10}, {120, 30}} ;
 
 	int n = sizeof(data)/sizeof(data[0]) ;
 
@@ -28,7 +46,7 @@ int main()
 	
 	int maxValue = knapSack(data,n,maxWeight) ;
 	
-	cout << "maxValue :" << maxValue ;
+	cout << "maxValue: " << maxValue ;
 	
 	return 0 ;
 }
